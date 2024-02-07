dce7da08238f4c53928c3b3c654527dc94057f21
Monvall <gersondaniel.monroy@gmail.com>
2024-02-06 20:10:38 -0600

    first commit

---
+++ app/src/main/java/com/example/monroyvalladares06_02_2024/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.monroyvalladares06_02_2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
